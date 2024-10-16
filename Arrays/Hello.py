5a535754f8e039e944fc604826969638c146f676
Praveena <pvvutla@gmail.com>
2024-10-16 16:27:01 -0700

    first hello python program

---
+++ Arrays/Hello.py
@@ -0,0 +1,18 @@
+print ("hello")
+
+#### Printing array
+
+a = [4,67,77] ## I am declaring an array with a name a
+# I am wiring an for loop to print the vlaues in the array a
+for x in a: 
+    print(x)
+
+
+
+
+
+
+
+
+
+
